de4f6cf6c2acc909eea6ff0060732d44482bb987
junbin <ghdwnsqls0@naver.com>
2024-05-01 09:24:26 +0900

    junbin

--- src/main/java/com/vue/SpringVueProjectApplication.java
+++ src/main/java/com/vue/SpringVueProjectApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringVueProjectApplication {
-
+	// 잘됨
 	public static void main(String[] args) {
 		SpringApplication.run(SpringVueProjectApplication.class, args);
 	}
